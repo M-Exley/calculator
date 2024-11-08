e95a9c75da4a7aeb5f52081e4576bec7ff1f5f6b
Macaylen <macaylen.exley@gmail.com>
2024-11-08 14:43:11 +0100

    initial

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# calculator
+# calculator project from Odin
+
+## uses mathematical functions and operators
+
+## store content of calculation in display
---
+++ script.js
@@ -0,0 +1,41 @@
+'use strict'
+
+const buttonAdd = document.querySelector('#add');
+const buttonSubtract = document.querySelector('#subtract');
+const numberOne = document.querySelector('#one');
+const numberTwo = document.querySelector('#two');
+const numberThree = document.querySelector('#three');
+const numberFour = document.querySelector('#four');
+const numberFive = document.querySelector('#five');
+const numberSix = document.querySelector('#six');
+const numberSeven = document.querySelector('#seven');
+const numberEight = document.querySelector('#eight');
+const numberNine = document.querySelector('#nine');
+const numberZero = document.querySelector('#zero');
+let input = 0;
+let total = 0;
+// array of numbers from nodelist buttons?
+
+function add(number) {
+    sum + 1;
+    return sum += number; 
+
+}
+
+function subtract(number) {
+    
+
+}
+
+function multiply() {
+
+}
+
+function divide() { 
+
+}
+
+buttonAdd.addEventListener('click', function(){
+    console.log("add button")
+})
+
